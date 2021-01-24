1fbf6f0466fc0d550ca808f3f8d1cb82bdf8943d
Lpppsea9 <lpppsea9@foxmail.com>
2021-01-24 16:35:08 +0800

    1/24

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -7,9 +7,22 @@
                 type="text" 
                 placeholder="输入城市名或拼音" 
             />
-        <div class="search-content">
+        <div 
+            class="search-content" 
+            ref="searchResult"
+            v-show="keyword"
+        >
             <ul>
-                <li>123</li>
+                <li 
+                    class="search-item border-bottom" 
+                    v-for="item of list"
+                    :key="item.id"
+                >
+                    {{item.name}}
+                </li>
+                <li class="search-item border-bottom" v-show="hasNoData">
+                    没有找到匹配数据
+                </li>
             </ul>
         </div>
         </div>
@@ -17,6 +30,7 @@
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
     name: 'CitySearch',
     props: {
@@ -25,9 +39,45 @@ export default {
     data () {
         return {
             keyword: '',
-            list: []
+            list: [],
+            timer: null
         }
+    },
+    // 把逻辑的运算都放在js里来完成，而模板里尽量保持简洁的语法
+    computed: {
+        hasNoData () {
+            return !this.list.length
+        }
+    },
+    // 节流
+    watch: {
+        // 当keyword改变时，会执行脚本
+        keyword () {
+            if (this.timer) {
+                clearTimeout(this.timer)
+            }
+            if (!this.keyword) {
+                this.list = []
+                return
+            }
+            // 当keyword改变时，隔100ms会执行
+            this.timer = setTimeout(() => {
+                const result = []
+                for (let i in this.cities) {
+                    this.cities[i].forEach((value) => {
+                        if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+                            result.push(value)
+                        }
+                    })
+                }
+                this.list = result
+            },100)
+        }
+    },
+    mounted () {
+        this.scroll = new Bscroll(this.$refs.searchResult)
     }
+
 }
 </script>
 
@@ -48,10 +98,16 @@ export default {
             color #666
     .search-content
         z-index 1
+        overflow: hidden
         position absolute
         top: 1.58rem
         left 0
         right 0
         bottom 0
-        background green
+        background #eee
+        .search-item
+            line-height .62rem
+            padding-left .2rem
+            background #fff
+            color #666
 </style>
