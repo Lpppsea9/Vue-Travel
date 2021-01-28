10edd1862ec628c7fed3d980909ae7c7387527a5
Lpppsea9 <lpppsea9@foxmail.com>
2021-01-29 07:06:35 +0800

    1/29

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,5 +1,15 @@
 <template>
-    <div>banner</div>
+    <div class="banner">
+        <img class="banner-img" src="http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_600x330_bf9c4904.jpg">
+        <div class="banner-info">
+            <div class="banner-title">
+                上海迪士尼乐园
+            </div>
+            <div class="banner-number">
+                <span>&#xe692;</span>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -9,5 +19,16 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+    .banner
+        height 0
+        padding-bottom 55%
+        overflow hidden
+        .banner-img
+            width 100%
+        .banner-info
+            position absolute
+            left 0
+            bottom 0
+            right 0
 
 </style>
